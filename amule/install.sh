0c2c651374c829ff141fbdc6870c9f62428040d8
Lucas Moreno <83520011+lucasgabmoreno@users.noreply.github.com>
2022-01-06 19:11:14 -0300

    Add files via upload

---
+++ amule/install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Remove old versions and trash
+bash uninstall.sh noremove
+
+# Install dependencies
+sudo apt-get install libcrypto++6 -y
+
+# Install
+PATH_AMULE="https://deb.sipwise.com/debian/pool/main/a/amule"
+PATH_WXWIDGETS="http://archive.ubuntu.com/ubuntu/pool/universe/w/wxwidgets3.0"
+COMMON="amule-common_2.3.2+git20200530.3a77afb-1_all.deb"
+LIBWXBASE="libwxbase3.0-0v5_3.0.5.1+dfsg-2_amd64.deb"
+LIBWXGTK="libwxgtk3.0-gtk3-0v5_3.0.5.1+dfsg-2_amd64.deb"
+AMULE="amule_2.3.2+git20200530.3a77afb-1+b1_amd64.deb"
+DAEMON="amule-daemon_2.3.2+git20200530.3a77afb-1+b1_amd64.deb"
+UTILS="amule-utils_2.3.2+git20200530.3a77afb-1+b1_amd64.deb"
+GUI="amule-utils-gui_2.3.2+git20200530.3a77afb-1+b1_amd64.deb"
+wget_dpkg_rm () {
+sudo wget "$1/$2"
+sudo dpkg -i "$2"
+sudo rm -rf "$2"
+}
+wget_dpkg_rm $PATH_AMULE $COMMON
+wget_dpkg_rm $PATH_WXWIDGETS $LIBWXBASE
+wget_dpkg_rm $PATH_WXWIDGETS $LIBWXGTK
+wget_dpkg_rm $PATH_AMULE $AMULE
+wget_dpkg_rm $PATH_AMULE $DAEMON
+wget_dpkg_rm $PATH_AMULE $UTILS
+wget_dpkg_rm $PATH_AMULE $GUI
+
+# Install aMule-EMC
+sudo apt-get install amule-emc -y
+
+# Final fixes
+sudo apt --fix-broken install -y
+
+# Create desktop launcher
+DESK_PATH=$(xdg-user-dir DESKTOP)
+sudo chmod +x /usr/share/applications/amule.desktop 
+cp /usr/share/applications/amule.desktop "$DESK_PATH/"
+sudo chmod +x "$DESK_PATH/"amule.desktop
+
+# Remove this insaller
+if [ ! -f .noremove ]; then rm -rf install.sh uninstall.sh; fi
+
+# Final message
+sudo echo "aMule installed!"
---
+++ amule/uninstall.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Hold dependencies
+sudo apt-mark hold libcrypto++6 2> /dev/null
+
+# Uninstall
+sudo apt remove amule* -y 2> /dev/null
+sudo apt purge amule* -y 2> /dev/null
+sudo apt autoremove -y 2> /dev/null
+sudo apt-mark unhold libcrypto++6 2> /dev/null
+
+# Remove trash
+sudo rm -rf ~/.aMule 2> /dev/null
+DESK_PATH=$(xdg-user-dir DESKTOP)
+sudo rm -rf "$DESK_PATH/amule.desktop" 2> /dev/null
+
+# Remove this uninstaller
+if ([ "$1" != "noremove" ] && [ ! -f .noremove ]); then rm -rf uninstall.sh; fi
+
+# Final message
+echo "aMule uninstalled!"
+
