60fca382d794a8420f7f7e97a35ce3f99605a025
Lucas Moreno <83520011+lucasgabmoreno@users.noreply.github.com>
2022-01-06 19:37:21 -0300

    Add files via upload

--- amule/uninstall.sh
+++ amule/uninstall.sh
@@ -11,6 +11,10 @@ sudo apt-mark unhold libcrypto++6 2> /dev/null
 
 # Remove trash
 sudo rm -rf ~/.aMule 2> /dev/null
+sudo rm -rf /usr/share/amule 2> /dev/null
+sudo rm -rf /etc/gufw/app_profiles/amule.gufw_app 2> /dev/null
+sudo rm -rf /usr/share/app-install/desktop/amule:amule.desktop 2> /dev/null
+sudo rm -rf /usr/share/app-install/desktop/amule-utils-gui:amulegui.desktop 2> /dev/null
 DESK_PATH=$(xdg-user-dir DESKTOP)
 sudo rm -rf "$DESK_PATH/amule.desktop" 2> /dev/null
 
