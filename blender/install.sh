5a115c186996bb8861d345cb0c26a3b3ffee9878
Lucas Moreno <83520011+lucasgabmoreno@users.noreply.github.com>
2022-01-06 19:24:01 -0300

    Add files via upload

--- blender/install.sh
+++ blender/install.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
-bash uninstall.sh
+# Remove old versions and trash
+bash uninstall.sh noremove
 
+# Install
 BLENDER="blender-3.0.0-linux-x64.tar.xz"
-PATH_OPT="/opt/blender"
-
+BLENDER_PATH=~/blender
 sudo wget "https://download.blender.org/release/Blender3.0/$BLENDER"
-
-sudo mkdir "$PATH_OPT"
-sudo tar -Jxf "$BLENDER" --strip-components=1 -C "$PATH_OPT"
-
-sudo sed -i 's|Exec=blender|Exec=/opt/blender/blender|g' "$PATH_OPT/blender.desktop"
-
-sudo cp "$PATH_OPT/blender.desktop" ~/.local/share/applications/
-sudo cp "$PATH_OPT/blender.svg" ~/.local/share/icons/
-sudo chown $USER:$USER ~/.local/share/applications/blender.desktop
-
+sudo mkdir "$BLENDER_PATH"
+sudo tar -Jxf "$BLENDER" --strip-components=1 -C "$BLENDER_PATH"
 sudo rm -rf "$BLENDER"
 
+# Create desktop launcher
+DESK_PATH=$(xdg-user-dir DESKTOP)
+USER_PATH=$(xdg-user-dir)
+sudo sed -i "s|Exec=blender|Exec=$USER_PATH/blender/blender|g" "$BLENDER_PATH/blender.desktop"
+sudo cp "$BLENDER_PATH/blender.desktop" ~/.local/share/applications/
+sudo cp "$BLENDER_PATH/blender.svg" ~/.local/share/icons/
+sudo chown $USER:$USER ~/.local/share/applications/blender.desktop
+sudo chmod +x ~/.local/share/applications/blender.desktop
+cp ~/.local/share/applications/blender.desktop "$DESK_PATH/"
+sudo chmod +x "$DESK_PATH/"blender.desktop
+
+# Remove this insaller
+if [ ! -f .noremove ]; then rm -rf install.sh uninstall.sh; fi
+
+# Final message
 sudo echo "Blender installed!"
--- blender/uninstall.sh
+++ blender/uninstall.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-sudo rm -rf /opt/blender
-sudo rm -rf ~/.local/share/applications/blender.desktop
-sudo rm -rf ~/.local/share/icons/blender.svg
+# Uninstall
+sudo apt remove blender* -y 2> /dev/null
+sudo apt purge blender* -y 2> /dev/null
+sudo apt autoremove -y 2> /dev/null
+sudo flatpak uninstall org.blender.Blender* -y 2> /dev/null
 
-sudo apt remove blender* -y
-sudo apt purge blender* -y
-sudo apt autoremove -y
+# Remove trash
+sudo rm -rf /opt/blender  2> /dev/null
+sudo rm -rf ~/blender 2> /dev/null
+sudo rm -rf ~/.local/share/applications/blender.desktop  2> /dev/null
+sudo rm -rf ~/.local/share/icons/blender.svg  2> /dev/null
+DESK_PATH=$(xdg-user-dir DESKTOP)
+sudo rm -rf "$DESK_PATH/blender.desktop" 2> /dev/null
 
-sudo flatpak uninstall org.blender.Blender* -y
+# Remove this uninstaller
+if ([ "$1" != "noremove" ] && [ ! -f .noremove ]); then rm -rf uninstall.sh; fi
 
+# Final message
 echo "Blender uninstalled!"
 
