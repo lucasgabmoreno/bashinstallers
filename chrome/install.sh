67b1acc9d7e79a133d9a9006ef5046ec7a702a24
Lucas Moreno <83520011+lucasgabmoreno@users.noreply.github.com>
2022-01-29 11:23:57 -0300

    chroome

--- chrome/install.sh
+++ chrome/install.sh
@@ -29,18 +29,14 @@ wget_dpkg_rm "$PATH_CHROME" "$CHROME"
 sudo apt --fix-broken install -y
 
 # Create desktop launcher
-DESK_PATH=$(xdg-user-dir DESKTOP)
 APP_PATH="/usr/share/applications/google-chrome.desktop"
 chmodown "$APP_PATH"
-sudo cp $APP_PATH "$DESK_PATH/"
-chmodown "$DESK_PATH/google-chrome.desktop"
-
 
 # Remove this insaller
 if [ ! -f .noremove ]; then rm -rf install.sh uninstall.sh; fi
 
 # Final message
-if [[ $(sudo apt list google-chrome*  2> /dev/null) == *"google-chrome"* ]]; then 
+if [[ $(sudo apt list--installed google-chrome*  2> /dev/null) == *"google-chrome"* ]]; then 
 sudo echo 'Google Chrome installed in '$(date -d @$((`date +%s`-$START_TIME)) -u +%H:%M:%S)
 else
 echo 'ERROR!!! Please copy the error message and paste them into https://github.com/lucasgabmoreno/bashinstallers/issues.'
