acb8211414b1002d7458e05cb19fe473cd614974
Lucas Moreno <83520011+lucasgabmoreno@users.noreply.github.com>
2022-09-27 19:04:58 -0300

    onlyoffice beta

--- onlyoffice beta/install.sh
+++ onlyoffice beta/install.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-# Functions
+SOFT_URL=$1
+SOFT_PACKAGE=onlyoffice
+
+# Permissions
 chmodown() {
 sudo chmod +x "$1"
 sudo chown $USER:$USER "$1"
 }
+
+# Download and install
+## Reemplzar $1/$2 por $1 y $2 por lo que va después de la URL
 wget_dpkg_rm () {
 sudo wget -t inf "$1/$2"
 if [ ! -f "$2" ]; then curl -L -O "$1/$2"; fi
@@ -15,7 +21,7 @@ sudo rm -rf inst.deb
 }
 
 if [ $USER == "root" ]; then
-echo "Don't run this bash file as root user"
+echo "Don't run as root user"
 else
 
 # Start count
@@ -43,34 +49,31 @@ sudo rm -rf ~/.config/onlyoffice* 2> /dev/null
 sudo rm -rf ~/.local/share/onlyoffice* 2> /dev/null
 sudo rm -rf ~/.local/share/applications/Desktopeditors* 2> /dev/null
 
-# Remove this uninstaller
-if ([ "$1" != "noremove" ] && [ ! -f .noremove ]); then
-    sudo rm -rf uninstall.sh
-fi
 
 # Final message
-if [[ $(sudo apt list onlyoffice*  2> /dev/null) != *"onlyoffice"* ]]; then
-    echo "OnlyOffice uninstalled!"
+if [[ $(sudo apt list "$SOFT_PACKAGE"*  2> /dev/null) != *"$SOFT_PACKAGE"* ]]; then
+    echo "Software uninstalled!"
 else
     echo 'ERROR!!! Please copy the error message and paste them into https://github.com/lucasgabmoreno/bashinstallers/issues'
 fi
 
 # INSTALLER
 
-wget_dpkg_rm 'https://download.onlyoffice.com/install/desktop/editors/linux/onlyoffice-desktopeditors_amd64.deb'
+if [ "$SOFT_URL" != "uninstall" ]; then
+
+wget_dpkg_rm "$SOFT_URL"
 
 # Final fixes
 sudo apt --fix-broken install -y
 
-# Create desktop launcher
+# Desktop launcher
 FROM_PATH="/usr/share/applications/onlyoffice-desktopeditors.desktop"
-chmodown "$FROM_PATH" 
-FROM_PATH_STR=$(paste $FROM_PATH)
 sudo cp "$FROM_PATH" onlyoffice-desktopeditors.desktop
 chmodown onlyoffice-desktopeditors.desktop
+FROM_PATH_STR=$(paste onlyoffice-desktopeditors.desktop)
 if [[ "$FROM_PATH_STR" != *"StartupWMClass"* ]]; then
     sudo sed -i '2 i\StartupWMClass=DesktopEditors' onlyoffice-desktopeditors.desktop
-fi
+fi # if not StartuoWMClass
 sudo sed -i 's|Keywords=Text;|Keywords=Text;winword;excel;powerpnt;|g' onlyoffice-desktopeditors.desktop
 sudo rm -rf "$FROM_PATH"
 sudo mv onlyoffice-desktopeditors.desktop /usr/share/applications/
@@ -79,13 +82,14 @@ sudo mv onlyoffice-desktopeditors.desktop /usr/share/applications/
 sudo rm -rf ~/.local/share/applications/Desktopeditors* 2> /dev/null
 
 # Remove this insaller
-SUdo rm -rf install.sh
+sudo rm -rf install.sh
 
 # Final message
-if [[ $(sudo apt list onlyoffice*  2> /dev/null) == *"onlyoffice"* ]]; then 
-    sudo echo 'OnlyOffice installed in '$(date -d @$((`date +%s`-$START_TIME)) -u +%H:%M:%S)
+if [[ $(sudo apt list "$SOFT_PACKAGE"*  2> /dev/null) == *"$SOFT_PACKAGE"* ]]; then 
+    sudo echo 'Software installed in '$(date -d @$((`date +%s`-$START_TIME)) -u +%H:%M:%S)
 else
     echo 'ERROR!!! Please copy the error message and paste them into https://github.com/lucasgabmoreno/bashinstallers/issues'
-fi
+fi # if installed
 
-fi
+fi # if not uninstall
+fi # if not root
