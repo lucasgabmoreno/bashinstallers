d04d1e559ee1c6f0bdfcdfb28bc35e8e60a993f1
Lucas Moreno <83520011+lucasgabmoreno@users.noreply.github.com>
2022-09-28 10:45:40 -0300

    Update install.sh

--- onlyoffice beta/install.sh
+++ onlyoffice beta/install.sh
@@ -2,6 +2,11 @@
 
 SOFT_URL=$1
 SOFT_PACKAGE=onlyoffice
+SOFT_KILL=DesktopEditors
+SOFT_FLATPACK=org.onlyoffice.desktopeditors
+DESK_PATH=$(xdg-user-dir DESKTOP) #/home/usernme/Dekstop/
+LAUNCHER_PATH="/usr/share/applications/onlyoffice-desktopeditors.desktop"
+LAUNCHER_DESK=${LAUNCHER_PATH##*/} #soft.desktop
 
 # Permissions
 chmodown() {
@@ -10,11 +15,12 @@ sudo chown $USER:$USER "$1"
 }
 
 # Download and install
-## Reemplzar $1/$2 por $1 y $2 por lo que va después de la URL
 wget_dpkg_rm () {
-sudo wget -t inf "$1/$2"
-if [ ! -f "$2" ]; then curl -L -O "$1/$2"; fi
-sudo mv "$2" inst.deb
+SOFT_URL=$1
+SOFT_DEB=${SOFT_URL##*/}
+sudo wget -t inf "$SOFT_URL"
+if [ ! -f "$SOFT_DEB" ]; then curl -L -O "$SOFT_URL"; fi
+sudo mv "$SOFT_DEB" inst.deb
 chmodown inst.deb
 sudo dpkg -i inst.deb
 sudo rm -rf inst.deb
@@ -32,29 +38,26 @@ START_TIME=`date +%s`
 # Remove old versions and trash
 
 # Close
-kill $(pidof DesktopEditors) 2> /dev/null
+kill $(pidof "$SOFT_KILL") 2> /dev/null
 
 # Uninstall
-sudo apt remove onlyoffice* -y 2> /dev/null
-sudo apt purge onlyoffice* -y 2> /dev/null
+sudo apt remove "$SOFT_PACKAGE"* -y 2> /dev/null
+sudo apt purge "$SOFT_PACKAGE"* -y 2> /dev/null
 sudo apt autoremove -y 2> /dev/null
-sudo flatpak uninstall org.onlyoffice.desktopeditors* -y 2> /dev/null
+sudo flatpak uninstall "$SOFT_FLATPACK"* -y 2> /dev/null
 
 # Remove trash
-FROM_PATH="/usr/share/applications/onlyoffice-desktopeditors.desktop"
-DESK_PATH=$(xdg-user-dir DESKTOP)
-sudo rm -rf "$DESK_PATH/"onlyoffice-desktopeditors.desktop 2> /dev/null
-sudo rm -rf $FROM_PATH
+sudo rm -rf "$DESK_PATH/$LAUNCHER_DESK" 2> /dev/null
+sudo rm -rf "$LAUNCHER_PATH"
 sudo rm -rf ~/.config/onlyoffice* 2> /dev/null
 sudo rm -rf ~/.local/share/onlyoffice* 2> /dev/null
 sudo rm -rf ~/.local/share/applications/Desktopeditors* 2> /dev/null
-
 
 # Final message
 if [[ $(sudo apt list "$SOFT_PACKAGE"*  2> /dev/null) != *"$SOFT_PACKAGE"* ]]; then
     echo "Software uninstalled!"
 else
-    echo 'ERROR!!! Please copy the error message and paste them into https://github.com/lucasgabmoreno/bashinstallers/issues'
+    echo 'Error!'
 fi
 
 # INSTALLER
@@ -67,16 +70,15 @@ wget_dpkg_rm "$SOFT_URL"
 sudo apt --fix-broken install -y
 
 # Desktop launcher
-FROM_PATH="/usr/share/applications/onlyoffice-desktopeditors.desktop"
-sudo cp "$FROM_PATH" onlyoffice-desktopeditors.desktop
-chmodown onlyoffice-desktopeditors.desktop
-FROM_PATH_STR=$(paste onlyoffice-desktopeditors.desktop)
-if [[ "$FROM_PATH_STR" != *"StartupWMClass"* ]]; then
-    sudo sed -i '2 i\StartupWMClass=DesktopEditors' onlyoffice-desktopeditors.desktop
+sudo cp "$LAUNCHER_PATH" "$LAUNCHER_DESK"
+chmodown "$LAUNCHER_DESK"
+LAUNCHER_DESK_STR=$(paste "$LAUNCHER_DESK")
+if [[ "$LAUNCHER_DESK_STR" != *"StartupWMClass"* ]]; then
+    sudo sed -i '2 i\StartupWMClass=DesktopEditors' "$LAUNCHER_DESK"
 fi # if not StartuoWMClass
-sudo sed -i 's|Keywords=Text;|Keywords=Text;winword;excel;powerpnt;|g' onlyoffice-desktopeditors.desktop
-sudo rm -rf "$FROM_PATH"
-sudo mv onlyoffice-desktopeditors.desktop /usr/share/applications/
+sudo sed -i 's|Keywords=Text;|Keywords=Text;winword;excel;powerpnt;|g' "$LAUNCHER_DESK"
+sudo rm -rf "$LAUNCHER_PATH" 
+sudo mv "$LAUNCHER_DESK" /usr/share/applications/
 
 # Remove trash
 sudo rm -rf ~/.local/share/applications/Desktopeditors* 2> /dev/null
@@ -88,7 +90,7 @@ sudo rm -rf install.sh
 if [[ $(sudo apt list "$SOFT_PACKAGE"*  2> /dev/null) == *"$SOFT_PACKAGE"* ]]; then 
     sudo echo 'Software installed in '$(date -d @$((`date +%s`-$START_TIME)) -u +%H:%M:%S)
 else
-    echo 'ERROR!!! Please copy the error message and paste them into https://github.com/lucasgabmoreno/bashinstallers/issues'
+    echo 'Errror!'
 fi # if installed
 
 fi # if not uninstall
