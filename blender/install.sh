b60b4c89b61690a1efbb61c3fcb96bc13d71418a
Lucas Moreno <83520011+lucasgabmoreno@users.noreply.github.com>
2022-10-27 19:25:15 -0300

    Update install.sh

--- blender/install.sh
+++ blender/install.sh
@@ -43,10 +43,10 @@ sudo flatpak uninstall "$SOFT_FLATPACK"* -y 2> /dev/null
 
 # Remove trash
 sudo rm -rf "/usr/share/icons/hicolor/128x128/apps/${HICOLOR##*/}" 2> /dev/null
-sudo rm -rf "~/blender" 2> /dev/null
+sudo rm -rf ~/blender 2> /dev/null
 sudo rm -rf $LAUNCHER_PATH 2> /dev/null
-sudo rm -rf "~/.local/share/icons/blender.svg"  2> /dev/null
-sudo rm -rf "~/.config/blender" 2> /dev/null
+sudo rm -rf "/usr/share/icons/blender.svg"  2> /dev/null
+sudo rm -rf ~/.config/blender 2> /dev/null
 DESK_PATH=$(xdg-user-dir DESKTOP)
 sudo rm -rf "$DESK_PATH/blender.desktop" 2> /dev/null
 
