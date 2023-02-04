127d9856d1dc6fbfc327e273145194d4d0a9230a
Lucas Moreno <83520011+lucasgabmoreno@users.noreply.github.com>
2023-02-03 21:39:09 -0300

    Update install.sh

--- apfs/install.sh
+++ apfs/install.sh
@@ -3,17 +3,7 @@
 sudo echo "Start"
 
 SOFT_URL=$1
-SOFT_PACKAGE='/usr/local/bin/slingscold'
-SOFT_KILL=slingscold
-DESK_PATH=$(xdg-user-dir DESKTOP) #/home/usernme/Dekstop/
-LAUNCHER_PATH="/usr/share/applications/slingscold.desktop"
-LAUNCHER_DESK=${LAUNCHER_PATH##*/} #soft.desktop
-
-# Permissions
-chmodown() {
-sudo chmod +x "$1"
-sudo chown $USER:$USER "$1"
-}
+SOFT_PACKAGE='/usr/bin/apfsutil'
 
 if [ $USER == "root" ]; then
 echo "Don't run as root user"
@@ -22,17 +12,11 @@ else
 # Start count
 START_TIME=`date +%s` 
 
-
 # UNINSTALLER
-# Remove old versions and trash
+# Remove
 
-# Close
-kill $(pidof "$SOFT_KILL") 2> /dev/null
-
-# Remove trash
-sudo rm -rf "$DESK_PATH/$LAUNCHER_DESK" 2> /dev/null
-sudo rm -rf "$LAUNCHER_PATH"
-sudo rm -rf "$SOFT_PACKAGE"
+sudo rm -rf /usr/bin/apfs-* 2> /dev/null
+sudo rm -rf /usr/bin/apfsutil 2> /dev/null
 
 # Final message
 if [ ! -e "$SOFT_PACKAGE" ]; then 
@@ -46,18 +30,22 @@ fi
 if [ "$SOFT_URL" != "uninstall" ]; then
 
 # Dependencies
-sudo apt-get install cmake libgee-0.8-dev libgnome-menu-3-dev cdbs valac libvala-*-dev libglib2.0-dev libwnck-3-dev libgtk-3-dev -y
+sudo apt install fuse libfuse3-dev bzip2 libbz2-dev cmake gcc g++ git libattr1-dev zlib1g-dev -y
 
 # Final fixes
 sudo apt --fix-broken install -y
 
 git clone $SOFT_URL
-cd slingscold
+cd apfs-fuse
+git submodule init
+git submodule update
+mkdir build
 cd build
 cmake ..
 make
-sudo make install
-sudo rm -rf "$(xdg-user-dir)/slingscold"
+sudo cp apfs-* /usr/bin/
+sudo cp apfsutil /usr/bin/
+sudo rm -rf "$(xdg-user-dir)/apfs-fuse"
 
 # Final message
 if [ -e "$SOFT_PACKAGE" ]; then 
